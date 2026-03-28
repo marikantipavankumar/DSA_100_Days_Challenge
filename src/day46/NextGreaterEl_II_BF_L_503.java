8f423240b9b9386ee916d0f93c90c398ffb215b1
Pavan Marikanti <marikantipavankumar0@gmail.com>
2026-03-28 09:10:05 +0530

    Corrected the changes in Leetcode 503 brute force approach

--- src/day46/NextGreaterEl_II_BF_L_503.java
+++ src/day46/NextGreaterEl_II_BF_L_503.java
@@ -9,35 +9,33 @@ public class NextGreaterEl_II_BF_L_503 {
         System.out.println("Next Greater Elements for each element in the given array is:"+ Arrays.toString(ans));
     }
 
-    public  static int[] nextGreaterElements(int[] nums) {
-            int[] ans = new int[nums.length];
+    public static int[] nextGreaterElements(int[] nums) {
+        int[] ans = new int[nums.length];
 
-            for(int i=0;i<ans.length;i++){
-                ans[i] = -1;
+        for(int i=0;i<ans.length;i++){
+            ans[i]=-1;
+        }
+
+        for(int i=0;i<nums.length;i++) {
+            boolean found = false;
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]>nums[i]){
+                    ans[i]=nums[j];
+                    found = true;
+                    break;
+
+                }
             }
-
-            for(int i=0;i<nums.length;i++) {
-
-                // Step 1: check right side
-                for(int j=i+1;j<nums.length;j++){
-                    if(nums[j] > nums[i]){
-                        ans[i] = nums[j];
+            if(found==false){
+                for(int r=0;r<i;r++){
+                    if(nums[r]>nums[i]){
+                        ans[i]=nums[r];
                         break;
                     }
                 }
-
-                // Step 2: check circular only if not found
-                if(ans[i] == -1){
-                    for(int r=0;r<i;r++){
-                        if(nums[r] > nums[i]){
-                            ans[i] = nums[r];
-                            break;
-                        }
-                    }
-                }
             }
-
-            return ans;
+        }
+        return ans;
         }
 }
 
