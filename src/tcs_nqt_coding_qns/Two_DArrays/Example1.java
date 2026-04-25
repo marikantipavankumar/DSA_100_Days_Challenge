1c4eae989c502952f3124ee9c71b47853560cc23
Pavan Marikanti <marikantipavankumar0@gmail.com>
2026-04-25 23:54:36 +0530

    Modified example 1:

--- src/tcs_nqt_coding_qns/Two_DArrays/Example1.java
+++ src/tcs_nqt_coding_qns/Two_DArrays/Example1.java
@@ -5,8 +5,7 @@ public class Example1 {
         int[][] arr = {
                 {1,2,3},
                 {4,5,6},
-                {7,8,9},
-                {10,22,22}
+                {7,8,9}
         };
 
 //        for(int i=0;i< arr.length;i++){
